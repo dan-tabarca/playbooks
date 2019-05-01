4d67900e706e8ab3ee2a006ec1a65b245e7f7894
dan-tabarca <root@danezu3c.mylabserver.com>
2019-05-01 09:22:37 +0000

    t4tst

--- git.py
+++ git.py
@@ -4,6 +4,7 @@ import subprocess
 # git config credential.helper store
 # git push https://github.com/repo.git
 
+subprocess.call(["git", "config", "credential.helper", "store"])
 subprocess.call(["git", "config", "--global", "credential.helper", "' cache --timeout 36000'"])
 subprocess.call(["git", "add", "."])
 message = raw_input("Commit your message: ")
