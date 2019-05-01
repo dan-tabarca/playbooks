cb18c15124c8ab4deb8835f235d8183bdef1737d
root <root@danezu3c.mylabserver.com>
2019-05-01 08:41:23 +0000

    test

--- push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-git add .
-#git reset set_pass.sh push.sh
-read -p "Commit message:" message
-set pass "vodafone20!"
-git commit -m $message
-git push
-exec /root/playbooks/set_pass.sh
-
