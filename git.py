dce33fc54c6a28074597eb4ba0583fb7d16dca18
dan-tabarca <root@danezu3c.mylabserver.com>
2019-05-01 09:16:38 +0000

    test

---
+++ git.py
@@ -0,0 +1,11 @@
+import subprocess
+
+# If the cache is gone, type manually: 
+# git config credential.helper store
+# git push https://github.com/repo.git
+
+subprocess.call(["git", "config", "--global", "credential.helper", "'cache --timeout 36000'"])
+subprocess.call(["git", "add", "."])
+message = raw_input("Commit your message: ")
+subprocess.call(["git", "commit", "-m", message])
+subprocess.Popen(["git", "push"])
