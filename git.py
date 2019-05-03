9fbf54b6bfa46b9ab9c37edaafb22c93f654e9a6
dan-tabarca <root@danezu3c.mylabserver.com>
2019-05-03 18:17:35 +0000

    test

--- git.py
+++ git.py
@@ -1,4 +1,8 @@
 import subprocess
+import getpass
+import os
+from glob import glob
+from github import Github
 
 # git config credential.helper store
 # git config --global credential.helper 'cache --timeout 36000'
@@ -11,22 +15,34 @@ import subprocess
 # curl https://api.github.com/users/dan-tabarca/repos?per_page=1000 |  grep -o 'git@[^\"]*' | cut -d '/' -f 2 | rev | cut -c 5- | rev
 
 def download_repos():
-#     download = os.system("curl https://api.github.com/users/dan-tabarca/repos?per_page=1000 | grep -o 'git@[^\"]*' | xargs -L1 git clone") 
-     p1=subprocess.call(["curl", "https://api.github.com/users/dan-tabarca/repos?per_page=1000"], stdout=subprocess.PIPE)
-     p2=subprocess.Popen(["grep", "-o 'git@[^\"]*'"], stdin=p1.stdout, shell=True, stdout=subprocess.PIPE)
-     p1.stdout.close()
-     result = p2.communicate()[0]
-     print(result)
+    dirs=[]
+    repos=[]
+    password = getpass.getpass("Insert your password for your GIT account: ")
+    g = Github("dan-tabarca", password) 
+    path=(os.getcwd())
+    full_path_of_dir=glob(path+"/*/")
+    for index in full_path_of_dir: 
+        dirs.append(index.split("/")[-2])
+    for repo in g.get_user().get_repos():
+        repos.append(repo.name)
+    if len(dirs)>0:
+        for directory in dirs:
+           for repo_value in repos:
+               if not repo_value in directory: 
+#                  print("Directory is:" + directory + "Repo is:" + repo_value)
+                   os.system("curl https://api.github.com/users/dan-tabarca/repos?per_page=1000 | grep -o 'git@github.com:dan-tabarca/'" + directory + ".git | xargs -L1 git clone")
+    else:
+        os.system("curl https://api.github.com/users/dan-tabarca/repos?per_page=1000 | grep -o 'git@[^\"]*' | xargs -L1 git clone")
 
 def prereq():
     req = subprocess.Popen(["git", "config", "--global", "credential.helper"], stdout=subprocess.PIPE)
     if not 'cache --timeout' in req.communicate()[0]:
         pwd = subprocess.Popen(["pwd"], stdout=subprocess.PIPE)
         full_path = pwd.communicate()[0].split("/")
-        repo = full_path[len(full_path)-1]
+        repository = full_path[len(full_path)-1]
         subprocess.call(["git", "config", "--global", "credential.helper", "cache"])
         subprocess.call(["git", "config", "--global", "credential.helper", "cache --timeout 36000"])
-        subprocess.call(["git", "push", "https://github.com/dan-tabarca/" + repo + ".git"])
+        subprocess.call(["git", "push", "https://github.com/dan-tabarca/" + repository + ".git"])
 
 def git():
     subprocess.call(["git", "add", "."])
@@ -35,7 +51,7 @@ def git():
     subprocess.call(["git", "commit", "-m", message])
     subprocess.call(["git", "push"])
 
-download_repos()
-#prereq()
-#git()
+#download_repos()
+prereq()
+git()
 
